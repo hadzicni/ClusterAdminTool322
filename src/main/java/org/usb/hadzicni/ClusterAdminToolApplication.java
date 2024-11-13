8bb8c95bce57067557dc9da77f8b89afccd02c02
hadzicni <nikola.hadzic@usb.ch>
2024-11-13 20:16:27 +0100

    initial commit: project setup

---
+++ src/main/java/org/usb/hadzicni/ClusterAdminToolApplication.java
@@ -0,0 +1,13 @@
+package org.usb.hadzicni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClusterAdminToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClusterAdminToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/usb/hadzicni/ClusterAdminToolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.usb.hadzicni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusterAdminToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
