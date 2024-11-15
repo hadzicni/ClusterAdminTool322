ec6b1f3b673817ff97ea5c531065ebc6769dc963
hadzicni <nikola.hadzic@usb.ch>
2024-11-15 08:56:42 +0100

    add redirect config from http to https

---
+++ src/main/java/org/usb/hadzicni/config/HttpToHttpsRedirectConfig.java
@@ -0,0 +1,25 @@
+package org.usb.hadzicni.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpToHttpsRedirectConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> factory.addAdditionalTomcatConnectors(createHttpConnector());
+    }
+
+    private Connector createHttpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(80); // HTTP port
+        connector.setSecure(false);
+        connector.setRedirectPort(443); // Redirect to HTTPS
+        return connector;
+    }
+}
